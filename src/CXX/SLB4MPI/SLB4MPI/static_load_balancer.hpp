79729021ff90ea4dd8abe682f02e0693ef6ea3f3
Igor S. Gerasimov <i.s.ger@ya.ru>
2024-10-19 13:18:57 +0200

    Add CXX static load balancer

---
+++ src/CXX/SLB4MPI/SLB4MPI/static_load_balancer.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#ifdef SLB4MPI_STATIC_LB
+#  error "SLB4MPI_STATIC_LB was previously defined"
+#else
+#  define SLB4MPI_STATIC_LB
+#endif
+
+#include "abstract_load_balancer.hpp"
+
+namespace SLB4MPI {
+  class StaticLoadBalancer final : public AbstractLoadBalancer {
+    private:
+      int64_t counter;
+    public:
+      /**
+       *
+       * @brief initialize values of static load balancer
+       *
+       * @param[in]           communicator   - MPI communicator on which load balancer will be used
+       * @param[in]           lower_bound    - lower bound of range
+       * @param[in]           upper_bound    - upper bound of range, upper_bound >= lower_bound
+       * @param[in, optional] min_chunk_size - minimal size of chank that can be associated with job, default: 1
+       * @param[in, optional] max_chunk_size - maximal size of chank that can be associated with job, default: upper_bound - lower_bound + 1
+       *
+       */
+      StaticLoadBalancer(const int communicator, const int64_t lower_bound, const int64_t upper_bound, const int64_t min_chunk_size = 1, const int64_t max_chunk_size = -1);
+      /**
+       *
+       * @brief d-tor of StaticLoadBalancer
+       *
+       */
+      virtual ~StaticLoadBalancer() {};
+      /**
+       *
+       * @brief get range to compute
+       * @note upper_bound can be only less than or equal StaticLoadBalancer.upper_bound
+       *       so, for last elements a batch with size less than min_chunk_size can be returned
+       *
+       * @param[out] lower_bound - lower bound of range to compute
+       * @param[out] upper_bound - upper bound of range to compute
+       * @return                 - is there something to compute?
+       *
+       */
+      virtual bool get_range(int64_t&, int64_t&);
+  };
+}
---
+++ src/CXX/static_load_balancer.cxx
@@ -0,0 +1,18 @@
+#include <SLB4MPI/static_load_balancer.hpp>
+
+#include <algorithm>
+
+SLB4MPI::StaticLoadBalancer::StaticLoadBalancer(const int communicator, const int64_t lower_bound, const int64_t upper_bound, const int64_t min_chunk_size, const int64_t max_chunk_size) :
+  SLB4MPI::AbstractLoadBalancer::AbstractLoadBalancer(communicator, lower_bound, upper_bound, min_chunk_size, max_chunk_size) {
+  this->counter = this->lower_bound + this->rank * this->max_chunk_size;
+}
+
+bool SLB4MPI::StaticLoadBalancer::get_range(int64_t& lower_bound, int64_t& upper_bound) {
+  lower_bound = this->counter;
+  upper_bound = std::min(lower_bound + this->max_chunk_size - 1, this->upper_bound);
+  this->counter += this->max_chunk_size * this->nranks;
+
+  bool to_compute = true;
+  if (lower_bound > this->upper_bound) to_compute = false;
+  return to_compute;
+}
