f1cde0657a15a8749ec3d49f0df178db17cd5aa4
Igor S. Gerasimov <i.s.ger@ya.ru>
2024-10-20 09:45:40 +0200

    Add C++ dynamic load balancer

---
+++ src/CXX/dynamic_load_balancer.cxx
@@ -0,0 +1,37 @@
+#include <SLB4MPI/dynamic_load_balancer.hpp>
+
+#include <algorithm>
+
+SLB4MPI::DynamicLoadBalancer::DynamicLoadBalancer(const int communicator, const int64_t lower_bound, const int64_t upper_bound, const int64_t min_chunk_size, const int64_t max_chunk_size) :
+  SLB4MPI::AbstractLoadBalancer::AbstractLoadBalancer(communicator, lower_bound, upper_bound, min_chunk_size, max_chunk_size) {
+  this->counter = this->lower_bound;
+
+#ifdef SLB4MPI_WITH_MPI
+  MPI_Win_create(&counter, sizeof(counter), sizeof(counter), MPI_INFO_NULL, this->communicator, &(this->window));
+  MPI_Barrier(this->communicator);
+#endif
+}
+
+SLB4MPI::DynamicLoadBalancer::~DynamicLoadBalancer() {
+#ifdef SLB4MPI_WITH_MPI
+  MPI_Win_free(&(this->window));
+#endif
+}
+
+bool SLB4MPI::DynamicLoadBalancer::get_range(int64_t& lower_bound, int64_t& upper_bound) {
+
+#ifdef SLB4MPI_WITH_MPI
+  MPI_Win_lock(MPI_LOCK_EXCLUSIVE, this->root, 0, this->window);
+  MPI_Fetch_and_op(&(this->min_chunk_size), &lower_bound, MPI_INT64_T, this->root, 0, MPI_SUM, this->window);
+  MPI_Win_unlock(this->root, this->window);
+#else
+  lower_bound = this->counter;
+  this->counter = this->counter + this->min_chunk_size;
+#endif
+
+  upper_bound = std::min(lower_bound + this->min_chunk_size - 1, this->upper_bound);
+
+  bool to_compute = true;
+  if (lower_bound > this->upper_bound) to_compute = false;
+  return to_compute;
+}
