f73f771a80d96a464ac564f30505474acf7040bd
Igor S. Gerasimov <i.s.ger@ya.ru>
2024-10-24 13:10:45 +0200

    Add example

---
+++ examples/CXX/example.cpp
@@ -0,0 +1,27 @@
+#include <SLB4MPI.hpp>
+
+#include <iostream>
+
+int main() {
+#ifdef WITH_MPI
+  MPI_Init();
+#else
+  const int MPI_COMM_WORLD = 0;
+#endif
+  {
+    int64_t bot, top;
+    SLB4MPI::RuntimeLoadBalancer lb("env", MPI_COMM_WORLD, 1, 100, 2, 4);
+    while (lb.get_range(bot, top)) {
+      std::cout << "Range [ " << bot << ", " << top << " ]" << std::endl;
+      for (int64_t i = bot; i <= top; i++) {
+        // do stuff
+      }
+    }
+    std::cout << "Done!" << std::endl;
+    // call descructor of RuntimeLoadBalancer here
+  }
+#ifdef WITH_MPI
+  MPI_Finalize();
+#endif
+  return 0;
+}
