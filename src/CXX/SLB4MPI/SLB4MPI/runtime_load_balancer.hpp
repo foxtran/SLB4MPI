44c9f101d832235566e0a7f533e19b395ade9bd8
Igor S. Gerasimov <foxtranigor@gmail.com>
2025-07-07 02:10:59 +0200

    Some fixes of doxygen format in C++ part

--- src/CXX/SLB4MPI/SLB4MPI/runtime_load_balancer.hpp
+++ src/CXX/SLB4MPI/SLB4MPI/runtime_load_balancer.hpp
@@ -20,12 +20,12 @@ namespace SLB4MPI {
        *
        * @brief initialize values of static load balancer
        *
-       * @param[in]           lbtype         - load balancer type
-       * @param[in]           communicator   - MPI communicator on which load balancer will be used
-       * @param[in]           lower_bound    - lower bound of range
-       * @param[in]           upper_bound    - upper bound of range, upper_bound >= lower_bound
-       * @param[in, optional] min_chunk_size - minimal size of chank that can be associated with job, default: 1
-       * @param[in, optional] max_chunk_size - maximal size of chank that can be associated with job, default: upper_bound - lower_bound + 1
+       * @param[in] lbtype         - load balancer type
+       * @param[in] communicator   - MPI communicator on which load balancer will be used
+       * @param[in] lower_bound    - lower bound of range
+       * @param[in] upper_bound    - upper bound of range, upper_bound >= lower_bound
+       * @param[in] min_chunk_size - minimal size of chank that can be associated with job, default: 1
+       * @param[in] max_chunk_size - maximal size of chank that can be associated with job, default: upper_bound - lower_bound + 1
        *
        */
       RuntimeLoadBalancer(const std::string& lbtype, const MPI_Comm communicator, const int64_t lower_bound, const int64_t upper_bound, const int64_t min_chunk_size = 1, const int64_t max_chunk_size = -1);
@@ -38,12 +38,12 @@ namespace SLB4MPI {
       /**
        *
        * @brief get range to compute
-       * @note upper_bound can be only less than or equal RuntimeLoadBalancer.upper_bound
-       *       so, for last elements a batch with size less than min_chunk_size can be returned
+       * @note `upper_bound` can be only less than or equal `RuntimeLoadBalancer.upper_bound`.
+       *       So, for last elements a batch with size less than `min_chunk_size` can be returned
        *
        * @param[out] lower_bound - lower bound of range to compute
        * @param[out] upper_bound - upper bound of range to compute
-       * @return                 - is there something to compute?
+       * @return                 - true if there is something to compute
        *
        */
       virtual bool get_range(int64_t&, int64_t&);
