3f8e39df3ccd8b80e4937ebf5597b2de7c27bc75
Igor S. Gerasimov <foxtranigor@gmail.com>
2025-07-07 02:19:33 +0200

    Some fixes of doxygen format in Fortran part

--- src/Fortran/local_static_load_balancer.f90
+++ src/Fortran/local_static_load_balancer.f90
@@ -22,11 +22,11 @@ contains
   !>
   !> @brief initialize local static load balancer
   !>
-  !> @param[in]           communicator   - MPI communicator on which load balancer will be used
-  !> @param[in]           lower_bound    - lower bound of range
-  !> @param[in]           upper_bound    - upper bound of range, upper_bound >= lower_bound
-  !> @param[in, optional] min_chunk_size - minimal size of chank that can be associated with job, default: 1
-  !> @param[in, optional] max_chunk_size - maximal size of chank that can be associated with job, default: upper_bound - lower_bound + 1
+  !> @param[in] communicator   - MPI communicator on which load balancer will be used
+  !> @param[in] lower_bound    - lower bound of range
+  !> @param[in] upper_bound    - upper bound of range, upper_bound >= lower_bound
+  !> @param[in] min_chunk_size - minimal size of chank that can be associated with job, default: 1
+  !> @param[in] max_chunk_size - maximal size of chank that can be associated with job, default: upper_bound - lower_bound + 1
   !>
   subroutine initialize(lb, communicator, lower_bound, upper_bound, min_chunk_size, max_chunk_size)
     class(local_static_load_balancer_t), intent(inout) :: lb
@@ -63,12 +63,12 @@ contains
   !>
   !> @brief get range to compute
   !>
-  !> @note upper_bound can be only less than or equal load_balancer_t%upper_bound
-  !>       so, for last elements a batch with size less than min_chunk_size can be returned
+  !> @note `upper_bound` can be only less than or equal `local_static_load_balancer_t%upper_bound`.
+  !>       So, for last elements a batch with size less than `min_chunk_size` can be returned
   !>
   !> @param[out] lower_bound - lower bound of range to compute
   !> @param[out] upper_bound - upper bound of range to compute
-  !> @return                 - is there something to compute?
+  !> @return                 - true if there is something to compute
   !>
   logical function get_range(lb, lower_bound, upper_bound) result(to_compute)
     class(local_static_load_balancer_t), intent(inout) :: lb
